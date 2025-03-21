f7f99cfe012056e5a0f29c2ad0ed3e390122c3e6
Arun Singh <arunsingh28aug.as@gmail.com>
2025-03-21 23:45:09 +0530

    add header and greeting components with basic styles and templates

--- firstApp/src/app/app.component.ts
+++ firstApp/src/app/app.component.ts
@@ -1,12 +1,20 @@
-import { Component } from '@angular/core';
+import { Component, input, signal } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
+import { Title } from '@angular/platform-browser';
+
+import { HeaderComponent } from './components/header/header.component';
+import { GreetingComponent } from "./components/greeting/greeting.component";
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports: [RouterOutlet, HeaderComponent, GreetingComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrl: './app.component.css',
 })
+
 export class AppComponent {
-  title = 'firstApp';
+  constructor(private titleService: Title) {
+    this.titleService.setTitle('Angular 18');
+  }
+  name = signal('Arun Pratap Singh');
 }
